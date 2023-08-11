452a1fc08d2cdb4e8187284526448ef49a5516cf
seonghun <sdzx0719@naver.com>
2023-08-11 14:53:15 +0900

    fix

--- src/main/java/leica/blog/service/CustomUserDetailsService.java
+++ src/main/java/leica/blog/service/CustomUserDetailsService.java
@@ -1,38 +1,38 @@
-//package leica.blog.service;
-//
-//import leica.blog.config.auth.jwt.JwtTokenProvider;
-//import leica.blog.entity.Member;
-//import leica.blog.repository.MemberRepository;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.security.core.userdetails.User;
-//import org.springframework.security.core.userdetails.UserDetails;
-//import org.springframework.security.core.userdetails.UserDetailsService;
-//import org.springframework.security.core.userdetails.UsernameNotFoundException;
-//import org.springframework.security.crypto.password.PasswordEncoder;
-//import org.springframework.security.core.authority.SimpleGrantedAuthority;
-//
-//import org.springframework.stereotype.Service;
-//
-//
-//@Service
-//@RequiredArgsConstructor
-//public class CustomUserDetailsService implements UserDetailsService {
-//
-//    private final MemberRepository memberRepository;
-//    private final PasswordEncoder passwordEncoder;
-//
-//    @Override
-//    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException{
-//        return memberRepository.findByUsername(username)
-//                .map(this::createUserDetails)
-//                .orElseThrow(() -> new UsernameNotFoundException("해당하는 유저가 없습니다."));
-//    }
-//
-//    private UserDetails createUserDetails(Member member) {
-//        return User.builder()
-//                .username(member.getUsername())
-//                .password(passwordEncoder.encode(member.getPassword()))
-//                .roles(member.getRoles().toArray(new String[0]))
-//                .build();
-//    }
-//}
+package leica.blog.service;
+
+import leica.blog.config.auth.jwt.JwtTokenProvider;
+import leica.blog.entity.Member;
+import leica.blog.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import org.springframework.stereotype.Service;
+
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException{
+        return memberRepository.findByUsername(username)
+                .map(this::createUserDetails)
+                .orElseThrow(() -> new UsernameNotFoundException("해당하는 유저가 없습니다."));
+    }
+
+    private UserDetails createUserDetails(Member member) {
+        return User.builder()
+                .username(member.getUsername())
+                .password(passwordEncoder.encode(member.getPassword()))
+                .roles(member.getRoles().toArray(new String[0]))
+                .build();
+    }
+}
