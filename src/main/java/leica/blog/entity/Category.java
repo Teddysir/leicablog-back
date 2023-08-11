324f3582cd0ddcdb7230898ab71f3fba0b0efb5a
seonghun <sdzx0719@naver.com>
2023-08-11 12:30:10 +0900

    test삭제

--- src/main/java/leica/blog/entity/Category.java
+++ src/main/java/leica/blog/entity/Category.java
@@ -14,7 +14,7 @@ import java.util.List;
 @NoArgsConstructor
 public class Category {
 
-    @Id @GeneratedValue
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "category_id")
     private Long id;
 
