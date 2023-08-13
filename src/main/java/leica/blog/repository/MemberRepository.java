02c13bf7ac676e8983e7713a9de44457f2b6050f
seonghun <sdzx0719@naver.com>
2023-08-13 18:57:01 +0900

    fixed1

--- src/main/java/leica/blog/repository/MemberRepository.java
+++ src/main/java/leica/blog/repository/MemberRepository.java
@@ -1,12 +1,12 @@
-package leica.blog.repository;
-
-import leica.blog.entity.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import java.util.Optional;
-
-@Repository
-public interface MemberRepository extends JpaRepository<Member, Long> {
-     Optional<Member> findByUsername(String username);
-}
+//package leica.blog.repository;
+//
+//import leica.blog.entity.Member;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.stereotype.Repository;
+//
+//import java.util.Optional;
+//
+//@Repository
+//public interface MemberRepository extends JpaRepository<Member, Long> {
+//     Optional<Member> findByUsername(String username);
+//}
